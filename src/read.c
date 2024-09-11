693eea9066d805bcfed2bc0ffc20fb677ebed1ff
Joao Pedro Horta Correia Silva Carvalho <jhorta-c@c2r3s6.42lisboa.com>
2024-09-11 22:41:47 +0100

    ola

--- src/read.c
+++ src/read.c
@@ -6,11 +6,35 @@
 /*   By: jhorta-c <jhorta-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 14:24:14 by jhorta-c          #+#    #+#             */
-/*   Updated: 2024/09/11 18:57:43 by jhorta-c         ###   ########.fr       */
+/*   Updated: 2024/09/11 22:40:54 by jhorta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+
+static int	*fill_vector(char *line, t_map *map)
+{
+	int	*vector;
+	int	i;
+	char	**str;
+
+	str = ft_split(line, ' ');
+	vector = malloc(sizeof(int) * (ft_arraylen(str)));
+	i = 0;
+	while (str[i])
+	{
+		vector[i] = ft_atoi(str[i]);
+		if (vector[i] > map->z_max)
+			map->z_max = vector[i];
+		if (vector[i] < map->z_min)
+			map->z_min = vector[i];
+		i++;
+	}
+	map->x_max = i;
+	ft_free_array(str);
+	return (vector);
+}
 
 t_map	read_map(char *file, int y_max)
 {
@@ -28,7 +52,7 @@ t_map	read_map(char *file, int y_max)
 	map.matrix = malloc(sizeof(int *) * y_max);
 	while ((str = get_next_line(fd)) == NULL)
 	{
-		map.matrix[i] = 
+		map.matrix[i] = fill_vector(str, &map);
 	}
 	
 }
